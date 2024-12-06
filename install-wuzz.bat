eacf741783934dccc201cd42fbd7b14a87f01a99
pranatahouse <pranatahouse@gmail.com>
2024-12-06 13:45:07 +0700

    update script to only install the wuzz style.

--- install-wuzz.bat
+++ install-wuzz.bat
@@ -1,7 +1,6 @@
 adb shell am force-stop net.osmand
 
 adb push wuzz.render.xml /sdcard/Android/data/net.osmand/files/rendering
-adb push pranatahouse.render.xml /sdcard/Android/data/net.osmand/files/rendering
 
 adb shell am start -n net.osmand/.plus.activities.MapActivity
 
