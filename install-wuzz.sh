358c1577349702367a5b66c3fd3939662e83ffa4
pranatahouse <pranatahouse@gmail.com>
2025-09-17 15:35:42 +0700

    Add shell script to update wuzz rendering on android via adb.

--- install-wuzz.sh
+++ install-wuzz.sh
@@ -1,6 +1,7 @@
 # pindah ke direktori skrip ini
 cd "$(dirname "$0")"
 
+# update rendering di osmand android
 adb shell am force-stop net.osmand
 adb push wuzz.render.xml /sdcard/Android/data/net.osmand/files/rendering
 adb shell am start -n net.osmand/.plus.activities.MapActivity
